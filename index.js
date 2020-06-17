0704ea2cbffc6e740584a6ac80e577941925c174
Matthew M <mbmoorin@gmail.com>
2020-06-17 18:39:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ function theBeatlesPlay(musicians, instruments) {
   for (let i = 0; i < musicians.length; i++) {
     array.push(`${musicians[i]} played ${instruments[i]}`);
   }
+  return array;
 }
 
